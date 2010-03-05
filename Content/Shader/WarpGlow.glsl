94d31e65e6cdee7d34b86d570937cb2434dead49
Matt Fichman <mfichman@stanford.edu>
2010-03-04 16:08:17 -0800

    Simplified shader

--- Content/Shader/WarpGlow.glsl
+++ Content/Shader/WarpGlow.glsl
@@ -1,7 +1,7 @@
 void phong(out vec4, out vec4, out vec4);
 
 uniform sampler2D glow_sampler; // Glow texture
-uniform sampler2D tex_sampler;
+//uniform sampler2D tex_sampler;
 uniform float time;
 uniform vec2 texscale;
 
@@ -13,9 +13,9 @@ void main() {
     
     vec2 texcoord = gl_TexCoord[0].st * texscale;
     
-    vec4 tex = texture2D(tex_sampler, texcoord);
+    //vec4 tex = texture2D(tex_sampler, texcoord);
     vec4 glow = texture2D(glow_sampler, texcoord);
-    vec4 color = (ambient + diffuse*tex + specular);
+    vec4 color = (ambient + diffuse * gl_FrontMaterial.diffuse/**tex*/ + specular);
     glow.a *= (cos(time) + 1.0)/2.0;
 
     // This is the final color of the pixel
