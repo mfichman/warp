9d195dc967c47c292233f5b23a8ca07ee309e4b9
Francesco Georg <fgeorg@stanford.edu>
2010-03-10 20:43:23 -0800

    added AI.lua

---
+++ Scripts/AI.lua
@@ -0,0 +1,26 @@
+AI = Object:new()
+
+
+function AI:trackFromSide(enemy)
+    local proj = Level:getSpineProjection(100)
+    local right = proj.forward:cross{0,1,0}
+    enemy:setPosition(proj.position + right * 50)
+    enemy:setVelocity{30,0,0}
+    enemy:setOrientation(proj.forward)
+
+    enemy.onTimeStep = function(self)
+        local target_direction = Level:getPlayerPosition() - enemy:getPosition()
+        target_direction:normalize()
+        local cur_orientation = enemy:getOrientation()
+        local normal = target_direction:cross(cur_orientation:getDirection())
+        -- cross product is ab*sin(theta)n = sin(theta)n
+        if (normal:squaredLength() > .1) then
+            rotation = Quaternion.new()
+            rotation:fromAngleAxis(normal, .1) -- adjust the angle between it and the player by .1 radians
+            cur_orientation = rotation * cur_orientation
+            enemy:setOrientation(cur_orientation)
+            enemy:setVelocity(cur_orientation * 25)
+        end
+        
+    end
+end
